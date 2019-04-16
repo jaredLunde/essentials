b136037656515eea1965ca6ea9178d691490ca4e
Jared Lunde <jared.lunde@gmail.com>
2019-04-16 17:17:39 -0600

    improves bench api

--- packages/benchmark/src/index.js
+++ packages/benchmark/src/index.js
@@ -1,14 +1,41 @@
 const boxen = typeof window === 'undefined' ? require('boxen') : void 0
 const now = typeof window === 'undefined' ?  require('performance-now') : performance.now.bind(performance)
 const chalk = typeof window === 'undefined' ? require('chalk').default : require('chalk-console')
-const prettyMs = typeof window === 'undefined' ? require('pretty-ms') : x => `${x < 1 ? x.toFixed(5) : Math.round(x)}ms`
-const prettyOps = x => Math.round(x).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+const prettyMs = require('pretty-ms')
+const prettyOps = x => {
+  x = x > 1 ? Math.round(x) : x.toFixed(4)
+  const parts = x.toString().split(".")
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+  return parts.join(".")
+}
 
-const bench = (fn, time = 1000, opt = {}) => {
-  let elapsed = 0.0, iterations = 0
-  const {before, after} = opt
+const bench = (label, fn) => {
+  if (typeof label !== 'string')
+    fn = label
 
-  while (elapsed < time) {
+  let elapsed = 0.0, iterations = 0, duration = 1000, before, after, calledOpt = false
+  const maybeFn = fn.call(this, {
+    before: cb => {
+      before = cb
+      calledOpt = true
+    },
+    after: cb => {
+      after = cb
+      calledOpt = true
+    },
+    duration: d => {
+      duration = d
+      calledOpt = true
+    }
+  })
+
+  if (calledOpt === true)
+    fn = maybeFn
+
+  if (typeof label !== 'string')
+    label = fn.toString()
+
+  while (elapsed < duration) {
     if (before) before.call()
 
     const start = now()
@@ -19,12 +46,12 @@ const bench = (fn, time = 1000, opt = {}) => {
     if (after) after.call()
   }
 
-  let output = fn.toString(), per = elapsed / iterations
+  let output = label, per = elapsed / iterations
   output += `\n\n${chalk.bold(prettyOps(1000.0 / per))}op/s`
   output += `\n${chalk.gray(prettyMs(per, {formatSubMs: per < 1}))}`
 
   if (typeof window === 'undefined')
-    output = boxen(output, {padding: 3})
+    output = boxen(output, {padding: 2})
 
   console.log(output)
 }
