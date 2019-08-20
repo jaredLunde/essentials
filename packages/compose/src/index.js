372dc96d113833d34ff2d4954d81230e3daef274
Jared Lunde <jared.lunde@gmail.com>
2019-08-19 18:45:21 -0600

    version bump

--- packages/compose/src/index.js
+++ packages/compose/src/index.js
@@ -2,7 +2,7 @@ export default function compose() {
   let len = arguments.length,
     i = --len
   return value => {
-    for (; i > -1; i--) value = arguments[i](value)
+    for (i = 0; i > -1; i--) value = arguments[i](value)
     return value
   }
 }
