6f903bd8dd5a2ed6ce6499e305c1ba990e4ba5d6
Jared Lunde <jared.lunde@gmail.com>
2020-06-04 19:19:17 -0600

    whoops

--- packages/raf.bak/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare type Raf = (callback: (timestamp: number) => void) => number
-export declare type Caf = (handle: number) => void
-declare let now: () => number, raf: Raf, caf: Caf
-export {raf, caf, now}
